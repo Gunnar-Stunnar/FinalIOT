a6160a1cb899e4435fd565f1c41d946c37130f9f
Gunnar-Stunnar <gjenserro@gmail.com>
2023-12-05 00:47:06 -0700

    Gunnar: rave 2

--- led.py
+++ led.py
@@ -5,13 +5,13 @@ redPin   = 11
 greenPin = 13
 bluePin  = 15
 
-def blink(pin):
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.HIGH)
+
 
 
 def main():
+
+    GPIO.setmode(GPIO.BOARD)
+
     GPIO.setup(redPin, GPIO.OUT)
     GPIO.setup(greenPin, GPIO.OUT)
     GPIO.setup(bluePin, GPIO.OUT)
@@ -25,18 +25,19 @@ def main():
     GREEN.start(1)
     BLUE.start(1)
 
-    for x in range(1,101):
-        GREEN.ChangeDutyCycle(x)
-        time.sleep(0.05)
-    
-    for x in range(1,101):
-        RED.ChangeDutyCycle(x)
-        time.sleep(0.05)
-    
-    for x in range(1,101):
-        BLUE.ChangeDutyCycle(x)
-        time.sleep(0.05)
-    
+    while True:
+        for x in range(1,101):
+            GREEN.ChangeDutyCycle(x)
+            time.sleep(0.05)
+        
+        for x in range(1,101):
+            RED.ChangeDutyCycle(x)
+            time.sleep(0.05)
+        
+        for x in range(1,101):
+            BLUE.ChangeDutyCycle(x)
+            time.sleep(0.05)
+        
 
 if __name__ == "__main__":
     try:
