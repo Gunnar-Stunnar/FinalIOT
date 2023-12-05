1b9a42573cfea494ad30e42f4a8b798b161d4b73
Gunnar-Stunnar <gjenserro@gmail.com>
2023-12-05 00:51:44 -0700

    Gunnar: rave 3

--- led.py
+++ led.py
@@ -25,19 +25,37 @@ def main():
     GREEN.start(1)
     BLUE.start(1)
 
+    sin_x, x = 1, 0
+    sin_y, y = 1, 0
+    sin_z, z = 1, 0
+
     while True:
-        for x in range(1,101):
-            GREEN.ChangeDutyCycle(x)
-            time.sleep(0.05)
+
         
-        for x in range(1,101):
-            RED.ChangeDutyCycle(x)
-            time.sleep(0.05)
+        GREEN.ChangeDutyCycle(x)
+        RED.ChangeDutyCycle(y)
+        BLUE.ChangeDutyCycle(z)
+
+        x += sin_x * 0.1
+        y += sin_y * 0.3
+        z += sin_z * 0.001
+
+        if x > 100:
+            sin_x = -1
+        elif x < 0:
+            sin_x = 1
         
-        for x in range(1,101):
-            BLUE.ChangeDutyCycle(x)
-            time.sleep(0.05)
-        
+        if y > 100:
+            sin_y = -1
+        elif y < 0:
+            sin_y = 1
+
+        if z > 100:
+            sin_z = -1
+        elif z < 0:
+            sin_z = 1
+
+        time.sleep(0.01)      
 
 if __name__ == "__main__":
     try:
