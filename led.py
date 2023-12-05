aa15ee37a818c8831b330e98e3ba18941a60904b
Gunnar-Stunnar <gjenserro@gmail.com>
2023-12-05 00:45:58 -0700

    Gunnar: pwm

--- led.py
+++ led.py
@@ -10,29 +10,36 @@ def blink(pin):
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
 
-def turnOff(pin):
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(pin, GPIO.OUT)
-	GPIO.output(pin, GPIO.LOW)
 
 def main():
+    GPIO.setup(redPin, GPIO.OUT)
+    GPIO.setup(greenPin, GPIO.OUT)
+    GPIO.setup(bluePin, GPIO.OUT)
 
-    flare = 0
-    while True:
-        if flare % 2 == 0:
-            blink(redPin)
-        
-        if  flare % 3 == 0:
-            blink(greenPin)
+    Freq = 100
+    RED = GPIO.PWM(redPin, Freq)
+    GREEN = GPIO.PWM(greenPin, Freq)
+    BLUE = GPIO.PWM(bluePin, Freq)
 
-        if  flare % 4 == 0:
-            blink(bluePin)
+    RED.start(100)
+    GREEN.start(1)
+    BLUE.start(1)
 
-        time.sleep(1)
-        turnOff(redPin)
-        turnOff(greenPin)
-        turnOff(bluePin)
-        flare += 1
+    for x in range(1,101):
+        GREEN.ChangeDutyCycle(x)
+        time.sleep(0.05)
+    
+    for x in range(1,101):
+        RED.ChangeDutyCycle(x)
+        time.sleep(0.05)
+    
+    for x in range(1,101):
+        BLUE.ChangeDutyCycle(x)
+        time.sleep(0.05)
+    
 
 if __name__ == "__main__":
-     main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
